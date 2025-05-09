0512db832c6ce636bb6a4d5df7bb0195394d4426
YaKhimYourDaddy <khiem140302@gmail.com>
2025-05-09 20:04:17 +0700

    abc

---
+++ src/components/hero-section.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { HeroHeader } from "@/components/hero6-header";
+
+export default function HeroSection() {
+  return (
+    <>
+      <HeroHeader />
+      <main className="overflow-hidden pt-20"></main>
+    </>
+  );
+}

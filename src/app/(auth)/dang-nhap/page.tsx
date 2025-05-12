dd19b084bc17c6cbff6b7dd056c72c71e1bdf77a
YaKhimYourDaddy <khiem140302@gmail.com>
2025-05-12 08:45:40 +0700

    google

---
+++ src/app/(auth)/dang-nhap/page.tsx
@@ -0,0 +1,22 @@
+import { signIn } from "@/auth";
+
+export default function SignIn() {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signIn("google");
+      }}
+    >
+      <button type="submit">Signin with Google</button>
+    </form>
+  );
+}
+
+// "use client";
+
+// import { signIn } from "next-auth/react";
+
+// export default function SignIn() {
+//   return <button onClick={() => signIn("google")}>login</button>;
+// }
